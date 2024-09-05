44551aea4d3612b3f640357c4771cff018412521
Justin Gauthier <itsme@justingauthier.com>
2024-09-04 22:28:54 -0400

    update OG meta data

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,9 +13,17 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html className={inter.className}>
       <head>
+        <title>Kitting Tool</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <meta property="og:title" content="Kitting Tool" />
+        <meta
+          property="og:description"
+          content="Optimize your character's gear with this lightweight gear kitting tool. Create, customize, and share the perfect loadouts for any class."
+        />
         <meta property="og:image" content="https://kittingtool.com/ogimage.jpg" />
+        <meta property="og:url" content="https://kittingtool.com" />
+        <meta property="og:type" content="website" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap"
           rel="stylesheet"
